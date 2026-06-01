72b3b221200abfd4766195cfe8490f0d6660f53b
damienloursac-ui <damien.loursac@gmail.com>
2026-06-01 18:26:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
 const CACHE_NAME = 'snowball-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
-
+const ASSETS = ['/snowball/', '/snowball/index.html', '/snowball/manifest.json', '/snowball/icons/icon-192.png', '/snowball/icons/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
